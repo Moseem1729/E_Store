1382eb658eae44f1c36a4db4b4721e90c57b5842
Moseem1729 <pmoseem@gmail.com>
2023-07-07 00:18:09 +0530

    upto user model GlobalExceptionHandler project done

---
+++ src/main/java/com/lcwd/electronic/store/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.lcwd.electronic.store.exceptions;
+
+import com.lcwd.electronic.store.dtos.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException e){
+
+        return new ResponseEntity<>(new ApiResponse(e.mes, false), HttpStatus.BAD_REQUEST);
+    }
+}
