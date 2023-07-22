d7de3487843f5c784c879481a373147985955827
Moseem1729 <pmoseem@gmail.com>
2023-07-22 20:39:20 +0530

    upto category model logger project done

---
+++ src/main/java/com/lcwd/electronic/store/entities/Product.java
@@ -0,0 +1,34 @@
+package com.lcwd.electronic.store.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    private String productId;
+    @Column()
+    private String title;
+    private String description;
+    private int price;
+    private int discountedPrice;
+    private int quantity;
+    private Date addedDate;
+    private boolean live;
+    private boolean stock;
+
+}
